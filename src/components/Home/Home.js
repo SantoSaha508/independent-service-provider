34e8f6deb42c0636ea813873ee7b97bc90a37bda
shanto <santo15-10959@diu.edu.bd>
2022-04-18 03:46:05 +0600

    add banar

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Home.css'
 
 const Home = () => {
     return (
         <div>
-            <h3>this is home</h3>
+            <div className='picture'>
+                <img src="https://images.ctfassets.net/u0haasspfa6q/2r74kh3TFj9jCWSdguDQBp/f30141a98cc7c0a9254df70e675ba20a/best-dslr-cameras-for-beginners" alt="no" />
+            </div>
         </div>
     );
 };
