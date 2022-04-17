4a44dc57f0b8b20e6553575361a6cac4d98b695f
shanto <santo15-10959@diu.edu.bd>
2022-04-18 03:29:10 +0600

    add react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Notfound from './components/Notfound/Notfound';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Service from './components/Service/Service';
+import Blogs from './components/Blogs/Blogs';
+import About from './components/About/About';
+import Notfound from './components/Notfound/Notfound'
 
 function App() {
   return (
     <div className="App">
-      
-      <Notfound></Notfound>
+      <Header>
+        
+      </Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/service' element={<Service></Service>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='*' element={<Notfound></Notfound>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className='photography'>
+            <div>
+            <h3>Photography world</h3>
+            </div>
+            <div>
+            <Link to='/home'>Home</Link>
+            <Link to="/service">Service</Link>
+            <Link to="/blogs">Blogs</Link>
+            <Link to="/about">About</Link>
+            <Link to="/notfound"></Link>
+            </div>
         </div>
     );
 };
